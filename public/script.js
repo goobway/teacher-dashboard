0b16045ae668b12fac404b291eaaa52a5a4823a7
goobway <calistagreenway@gmail.com>
2023-05-03 14:53:29 -0400

    matrix display

--- public/script.js
+++ public/script.js
@@ -2,7 +2,30 @@
 // Call loadNav to load the navigation menu
 loadNav();
 
-let studentData;
+function matrixToDataURL(matrix) {
+  const canvas = document.createElement('canvas');
+  const ctx = canvas.getContext('2d');
+  const size = 32;
+
+  canvas.width = size;
+  canvas.height = size;
+  const imageData = ctx.createImageData(size, size);
+
+  for (let i = 0; i < matrix.length; i++) {
+    for (let j = 0; j < matrix[i].length; j++) {
+      const pixelIndex = (i * size + j) * 4;
+      const value = matrix[i][j] * 255;
+
+      imageData.data[pixelIndex] = 255 - value;
+      imageData.data[pixelIndex + 1] = 255 - value;
+      imageData.data[pixelIndex + 2] = 255 - value;
+      imageData.data[pixelIndex + 3] = 255;
+    }
+  }
+
+  ctx.putImageData(imageData, 0, 0);
+  return canvas.toDataURL();
+}
 
 function getStudentDetailsById(id) {
   const studentDetails = [
