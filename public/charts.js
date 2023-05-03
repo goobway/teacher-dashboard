a59b2cb4f23b8b9f662a7eef7a3509b4b399f258
goobway <calistagreenway@gmail.com>
2023-05-03 17:32:51 -0400

    new students

--- public/charts.js
+++ public/charts.js
@@ -1,3 +1,5 @@
+loadNav();
+
 fetch('/values')
   .then(response => response.text())
   .then(text => {
@@ -6,21 +8,32 @@ fetch('/values')
   })
   .then(data => {
     // Parse the data and create the chart
-    const chartData = data.data.map(student => ({
-      x: student.confidence,
-      y: student.prompt,
-      borderColor: `hsl(${student.id * 36}, 70%, 50%)`,
-      pointBackgroundColor: `hsl(${student.id * 36}, 70%, 50%)`,
-      pointRadius: 5,
-      pointHoverRadius: 8,
-    }));
+    const chartData = {};
 
+    // Process student data
+    data.data.forEach(student => {
+      if (!chartData[student.id]) {
+        chartData[student.id] = {
+          label: student.id,
+          data: [],
+          borderColor: `hsl(${student.id * 36}, 70%, 50%)`,
+          pointBackgroundColor: `hsl(${student.id * 36}, 70%, 50%)`,
+          pointRadius: 5,
+          pointHoverRadius: 8,
+        };
+      }
+
+      chartData[student.id].data.push({
+        x: student.confidence,
+        y: student.prompt,
+      });
+    });
+
+    // Create the chart
     new Chart('scatter-chart', {
       type: 'scatter',
       data: {
-        datasets: [{
-          data: chartData,
-        }],
+        datasets: Object.values(chartData),
       },
       options: {
         scales: {
